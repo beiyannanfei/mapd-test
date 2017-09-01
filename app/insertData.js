ee68cd77a85dfc505d33b3359adfd37265d2157d
wangyanqiang <wangyanqiang@sensoro.com>
2017-09-01 18:48:50 +0800

    update

--- app/insertData.js
+++ app/insertData.js
@@ -4,7 +4,7 @@
 const client = require("./index");
 
 client().then(sess => {
-	let sqlStr = 'insert into wifipix values (106.804220692905,	95.0403826300024, 281),(80.804220692905,	35.0403826300024, 132);';
+	let sqlStr = 'insert into wifipix values (106.804220692905,	95.0403826300024, 281), (80.804220692905, 35.0403826300024, 132);';
 	sess.query(sqlStr, {}, (err, response) => {
 		if (!!err) {
 			return console.log("query err: %j", err.message || err);
